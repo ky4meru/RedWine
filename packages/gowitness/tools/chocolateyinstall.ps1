de1a30261df53c6a228d0ea75909aa9b9aa8fee7
ky4meru <ky4meru@pm.me>
2025-08-28 14:18:38 +0200

    gowitness package

---
+++ packages/gowitness/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'Gowitness'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/sensepost/gowitness/releases/download/3.0.5/gowitness-3.0.5-windows-amd64.exe'
+$exePath = $(Join-Path $toolsDir "$packageName.exe")
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $exePath -Url $url
