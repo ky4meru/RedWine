fe6ceaf5fa3373cf36ac567a2ebf8a3f99e58258
ky4meru <ky4meru@pm.me>
2025-08-28 11:12:26 +0200

    azurehound package

---
+++ packages/azurehound/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = "AzureHound"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/SpecterOps/AzureHound/releases/download/v2.7.1/AzureHound_v2.7.1_windows_amd64.zip'
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath "$toolsDir\$packageName.zip" -Url $url
+Get-ChocolateyUnzip -FileFullPath "$toolsDir\$packageName.zip" -Destination $toolsDir
--- start.ps1
+++ start.ps1
@@ -36,6 +36,7 @@ $RedWinePackagesToInstall = @(
     # Then, install packages.
     "abuseazureapipermissions"
     "adsearch"
+    "azurehound"
     "bloodyad"
     "domainpasswordspray"
     "godpotato"
