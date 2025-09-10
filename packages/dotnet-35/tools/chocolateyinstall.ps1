05c0c586cff640d6f2fcf2740d2c30e27e67841a
ky4meru <ky4meru@pm.me>
2025-09-10 15:06:27 +0200

    modify valid exit codes for enabling and disabling .net 3.5

--- packages/dotnet-35/tools/chocolateyinstall.ps1
+++ packages/dotnet-35/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 Start-ChocolateyProcessAsAdmin -ExeToRun "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" -Statements "modify --productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.16.Release --add Microsoft.VisualStudio.Workload.ManagedDesktop --quiet --norestart"
 Start-ChocolateyProcessAsAdmin -ExeToRun "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" -Statements "modify --productId Microsoft.VisualStudio.Product.Community --channelId VisualStudio.16.Release --add Microsoft.Net.Component.3.5.DeveloperTools --quiet --norestart"
-Start-ChocolateyProcessAsAdmin -ExeToRun "C:\Windows\system32\Dism.exe" -Statements "/Online /Enable-Feature /FeatureName:NetFx3 /All /NoRestart"
+Start-ChocolateyProcessAsAdmin -ExeToRun "C:\Windows\system32\Dism.exe" -Statements "/Online /Enable-Feature /FeatureName:NetFx3 /All /NoRestart" -ValidExitCodes @(0, 3010)
