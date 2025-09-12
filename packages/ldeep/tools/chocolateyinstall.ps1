3a59b2cd6b648e8f5fa6a4d9ab3fa7f9767d3c00
ky4meru <ky4meru@pm.me>
2025-09-12 23:19:55 +0200

    ldeep package

---
+++ packages/ldeep/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+    packageName = $env:ChocolateyPackageTitle
+    fileFullPath = $(Join-Path $toolsDir "$env:ChocolateyPackageTitle.exe")
+    url = "https://github.com/franc-pentest/ldeep/releases/download/$env:ChocolateyPackageVersion/ldeep_windows-amd64.exe"
+}
+
+Get-ChocolateyWebFile @packageArgs
