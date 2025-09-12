bc3b54daf7d7dd27266ca2b5554dcaafe6f5a3bb
ky4meru <ky4meru@pm.me>
2025-09-12 22:36:34 +0200

    fix coercer version

--- packages/coercer/tools/chocolateyinstall.ps1
+++ packages/coercer/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $temp = $Env:TEMP
 Install-ChocolateyEnvironmentVariable -VariableName "TMP" -VariableValue "$Env:ChocolateyInstall\temp" -VariableType "User"
 Install-ChocolateyEnvironmentVariable -VariableName "TEMP" -VariableValue "$Env:ChocolateyInstall\temp" -VariableType "User"
 
-Start-ChocolateyProcessAsAdmin -ExeToRun "python" -Statements "-m pipx install git+$url@v$env:ChocolateyPackageVersion"
+Start-ChocolateyProcessAsAdmin -ExeToRun "python" -Statements "-m pipx install git+$url@$env:ChocolateyPackageVersion"
 
 Install-ChocolateyEnvironmentVariable -VariableName "TMP" -VariableValue "$tmp" -VariableType "User"
 Install-ChocolateyEnvironmentVariable -VariableName "TEMP" -VariableValue "$temp" -VariableType "User"
