837cfd9f5e98ef9c449b1a9d7b6de3853f8e43ab
ky4meru <ky4meru@pm.me>
2025-09-13 09:57:43 +0200

    adexplorersnapshot package

---
+++ packages/adexplorersnapshot/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url = 'https://github.com/c3c/ADExplorerSnapshot.py'
+
+$tmp = $Env:TMP
+$temp = $Env:TEMP
+
+Install-ChocolateyEnvironmentVariable -VariableName "TMP" -VariableValue "$Env:ChocolateyInstall\temp" -VariableType "User"
+Install-ChocolateyEnvironmentVariable -VariableName "TEMP" -VariableValue "$Env:ChocolateyInstall\temp" -VariableType "User"
+
+Start-ChocolateyProcessAsAdmin -ExeToRun "python" -Statements "-m pipx install git+$url@dd9bad0de50f4d7fdf9bcd740cc1b95c90818429"
+
+Install-ChocolateyEnvironmentVariable -VariableName "TMP" -VariableValue "$tmp" -VariableType "User"
+Install-ChocolateyEnvironmentVariable -VariableName "TEMP" -VariableValue "$temp" -VariableType "User"
