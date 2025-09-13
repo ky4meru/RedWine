954128640f554c8948485ad69288e6142d38f991
ky4meru <ky4meru@pm.me>
2025-09-13 10:17:23 +0200

    krbrelayex package

---
+++ packages/krbrelayex/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+    packageName = $env:ChocolateyPackageTitle
+    fileFullPath = $(Join-Path $toolsDir "$env:ChocolateyPackageTitle.exe")
+    url = "https://github.com/decoder-it/KrbRelayEx/releases/download/v$env:ChocolateyPackageVersion/KrbRelayEx.exe"
+}
+
+Get-ChocolateyWebFile @packageArgs
