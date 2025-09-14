8b4813cd9c47e87efe30d257053992717bcf56bf
ky4meru <ky4meru@pm.me>
2025-09-14 09:52:45 +0200

    ms cpp build tools dependency for netexec and donpapi

--- packages/donpapi/tools/chocolateyinstall.ps1
+++ packages/donpapi/tools/chocolateyinstall.ps1
@@ -1,12 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$url = 'https://github.com/login-securite/DonPAPI'
+$tag = '61db37aaea1e1c9e9b6a02c640bf59f2a20840bc'
+
 $tmp = $Env:TMP
 $temp = $Env:TEMP
 
 Install-ChocolateyEnvironmentVariable -VariableName "TMP" -VariableValue "$Env:ChocolateyInstall\temp" -VariableType "User"
 Install-ChocolateyEnvironmentVariable -VariableName "TEMP" -VariableValue "$Env:ChocolateyInstall\temp" -VariableType "User"
 
-Start-ChocolateyProcessAsAdmin -ExeToRun "python" -Statements "-m pipx install donpapi==$env:ChocolateyPackageVersion"
+Start-ChocolateyProcessAsAdmin -ExeToRun "python" -Statements "-m pipx install git+$url@$tag"
 
 Install-ChocolateyEnvironmentVariable -VariableName "TMP" -VariableValue "$tmp" -VariableType "User"
 Install-ChocolateyEnvironmentVariable -VariableName "TEMP" -VariableValue "$temp" -VariableType "User"
--- packages/netexec/tools/chocolateyuninstall.ps1
+++ packages/netexec/tools/chocolateyuninstall.ps1
@@ -1,3 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+Remove-Item -Recurse -Force $env:NXC_PATH
+Uninstall-ChocolateyEnvironmentVariable -VariableName "NXC_PATH" -VariableType "User"
+
 Start-ChocolateyProcessAsAdmin -ExeToRun "python" -Statements "-m pipx uninstall netexec -y"
