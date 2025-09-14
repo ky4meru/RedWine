223670a959bf607fbe5a533982506583f8f2428d
ky4meru <ky4meru@pm.me>
2025-09-14 18:50:37 +0200

    try redwine extension with zip

--- packages/rustscan/tools/chocolateyinstall.ps1
+++ packages/rustscan/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-    packageName = $env:ChocolateyPackageTitle
-    unzipLication = $toolsDir
+    name = "$env:ChocolateyPackageTitle"
     url = "https://github.com/bee-san/RustScan/releases/download/$env:ChocolateyPackageVersion/x86_64-windows-rustscan.exe.zip"
+    path = "$toolsDir"
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Install-RedWineArchivePackage @packageArgs
