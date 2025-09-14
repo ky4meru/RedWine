6093d93d63d12cdf4d9cb67ab94fa7da1f3e35aa
ky4meru <ky4meru@pm.me>
2025-09-14 14:18:25 +0200

    test redwine extension with bloodyad

--- packages/bloodyad/tools/chocolateyinstall.ps1
+++ packages/bloodyad/tools/chocolateyinstall.ps1
@@ -1,11 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-
 $packageArgs = @{
     packageName = $env:ChocolateyPackageTitle
-    fileFullPath = $(Join-Path $toolsDir "$env:ChocolateyPackageTitle.exe")
     url = "https://github.com/CravateRouge/bloodyAD/releases/download/v$env:ChocolateyPackageVersion/bloodyAD.exe"
 }
 
-Get-ChocolateyWebFile @packageArgs
+Install-RedWinePortablePackage @packageArgs
