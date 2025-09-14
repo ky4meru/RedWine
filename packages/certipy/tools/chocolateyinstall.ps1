5a544430dd2df15dccccffcf69ce98df84aa773d
ky4meru <ky4meru@pm.me>
2025-09-14 19:41:49 +0200

    certipy package

---
+++ packages/certipy/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    name = "$env:ChocolateyPackageTitle"
+    url = "https://github.com/ly4k/Certipy/releases/download/$env:ChocolateyPackageVersion/Certipy.exe"
+    path = $(Join-Path $toolsDir "$env:ChocolateyPackageTitle.exe")
+}
+
+Install-RedWinePortablePackage @packageArgs
